e7ea7429aadc4b93b654d50f13747181a1c0d2ca
yue.chang <r3399r@gmail.com>
2021-11-19 02:01:26 +0800

    remove github action

--- index.ts
+++ index.ts
@@ -1,2 +1,5 @@
-export { DbService } from "./src/services/DbService";
-export { entity, primaryAttribute, generateData, generateRelationalData } from "./src/util/DbHelper";
+import 'reflect-metadata';
+export { DbService } from "src/services/DbService";
+export { entity, primaryAttribute, generateData, generateRelationalData } from "src/util/DbHelper";
+export { errorOutput, LambdaOutput, successOutput } from 'src/util/LambdaOutput';
+export { LambdaContext } from 'src/lambda/LambdaContext';
