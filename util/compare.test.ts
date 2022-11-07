d0be46fc1a009c9e71005d0805ae45adab9457a3
Yue <yue@potentia.asia>
2022-11-07 16:04:54 +0800

    improve compare usage

--- util/compare.test.ts
+++ util/compare.test.ts
@@ -11,5 +11,27 @@ describe('sort', () => {
     expect(
       [{ id: 'id5' }, { id: 'id1' }, { id: 'id10' }].sort(compare('id'))
     ).toStrictEqual([{ id: 'id1' }, { id: 'id10' }, { id: 'id5' }]);
+    expect(
+      [
+        { date: '2022-10-14T09:49:00.000Z' },
+        { date: '2022-10-15T09:49:00.000Z' },
+        { date: '2022-10-13T09:49:00.000Z' },
+      ].sort(compare('date'))
+    ).toStrictEqual([
+      { date: '2022-10-13T09:49:00.000Z' },
+      { date: '2022-10-14T09:49:00.000Z' },
+      { date: '2022-10-15T09:49:00.000Z' },
+    ]);
+    expect(
+      [
+        { date: '2022-10-14T09:49:00.000Z' },
+        { date: '2022-10-15T09:49:00.000Z' },
+        { date: '2022-10-13T09:49:00.000Z' },
+      ].sort(compare('date', 'desc'))
+    ).toStrictEqual([
+      { date: '2022-10-15T09:49:00.000Z' },
+      { date: '2022-10-14T09:49:00.000Z' },
+      { date: '2022-10-13T09:49:00.000Z' },
+    ]);
   });
 });
--- util/compare.ts
+++ util/compare.ts
@@ -1,7 +1,10 @@
-export function compare<T>(key: keyof T): (a: T, b: T) => number {
+export function compare<T>(
+  key: keyof T,
+  order: 'asc' | 'desc' = 'asc'
+): (a: T, b: T) => number {
   return (a: T, b: T) => {
-    if (a[key] < b[key]) return -1;
-    if (a[key] > b[key]) return 1;
+    if (a[key] < b[key]) return order === 'asc' ? -1 : 1;
+    if (a[key] > b[key]) return order === 'asc' ? 1 : -1;
 
     return 0;
   };
