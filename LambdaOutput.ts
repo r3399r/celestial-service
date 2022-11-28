f226f4896c562e0663a6ff4401dc127d174b6ead
Yue <yue@potentia.asia>
2022-11-28 16:49:37 +0800

    consider pagination

--- LambdaOutput.ts
+++ LambdaOutput.ts
@@ -1,11 +1,25 @@
 import { HttpError } from './error/HttpError';
 import { LambdaOutput } from './model/Lambda';
+import { Pagination } from './model/Pagination';
 
 export function successOutput<T>(res: T): LambdaOutput {
+  if ('paginate' in res && 'data' in res) {
+    const pagination = res as unknown as Pagination<T>;
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Required for CORS support to work,
+        'x-pagination-count': pagination.paginate.count.toString(),
+        'x-pagination-limit': pagination.paginate.limit.toString(),
+        'x-pagination-offset': pagination.paginate.offset.toString(),
+      },
+      body: JSON.stringify(pagination.data),
+    };
+  }
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work,
     },
     body: JSON.stringify(res),
   };
